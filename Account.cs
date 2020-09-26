4ad3b94ee1632140bb3a420902b9967002ede791
Petr <ferel2015@yandex.ru>
2020-09-26 18:42:03 +0300

    Добавлен класс Account

---
+++ Account.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PeopleStaff
+{
+    class Account
+    {
+        // Ограничение обобщений 
+        public int  Id { get; set; }
+        public int Sum { get; set; }
+
+        public Account(int id)
+        {
+            Id = id;
+        }
+
+    }
+}
