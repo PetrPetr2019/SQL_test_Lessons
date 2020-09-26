fdb716e8a3fc43c2dfe25a19a8bcc43c01df570f
Petr <ferel2015@yandex.ru>
2020-09-26 18:52:43 +0300

    Добавлет типизтрованный класс class Transaction<T>where T:Account

---
+++ Transaction.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PeopleStaff
+{
+    class Transaction<T>where T:Account
+    {
+        public T FromAccount { get; set; }
+        public T ToAccount { get; set; }
+        public int Sum { get; set; }
+
+        public  void Execute()
+        {
+            if (FromAccount.Sum > Sum)
+            {
+                FromAccount.Sum -= Sum;
+                ToAccount.Sum += Sum;
+                Console.WriteLine($"Счет:{FromAccount.Id}:{FromAccount.Sum}$счет:{ToAccount.Id}:{ToAccount.Sum}");
+            }
+            else
+            {
+                Console.WriteLine($"не достаточно денег:{FromAccount.Id}");
+            }
+        }
+
+    }
+}
